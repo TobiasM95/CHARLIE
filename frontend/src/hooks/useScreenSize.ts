47c3ac9e9c1eebd3f29667c1e6db873ca527fdd1
Lars <lars.lissek@tu-dortmund.de>
2023-04-15 19:55:31 +0200

    lint

--- frontend/src/hooks/useScreenSize.ts
+++ frontend/src/hooks/useScreenSize.ts
@@ -2,11 +2,11 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { mobileBreakpoint } from "../Settings/Constants";
 
 interface ScreenSize {
-    isMobile: boolean;
+  isMobile: boolean;
 }
 
 export const useScreenSize = (): ScreenSize => {
-    return({
-        isMobile: useMediaQuery(`(max-width: ${mobileBreakpoint})`)
-    });
-}
+  return {
+    isMobile: useMediaQuery(`(max-width: ${mobileBreakpoint})`)
+  };
+};
